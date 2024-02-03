c190e59f8a51629ece54c72d22d38c0477694a65
nikolyan-nki <nikolyan2004@gmail.com>
2024-02-03 20:23:45 +0300

    tapping

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 nasm -f elf32 sys/boot/boot.asm -o boot.o
-gcc -m32 -c sys/kernel/header.c -o krnl.o
-ld -m elf_i386 -T link.ld -o kernel boot.o krnl.o
+gcc -fno-stack-protector -m32 -c sys/kernel/header.c -o bin/kc.o
+ld -m elf_i386 -T link.ld -o kernel boot.o bin/kc.o
 qemu-system-i386 -kernel kernel -serial stdio
 
 
